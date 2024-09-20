583bc602617ae3183e12fc9db6ef476a602817c9
Nikita Gavrilov <nikgavrilov0210@gmail.com>
2024-09-20 13:00:11 +0400

    Создал навигацию и Toolbar

---
+++ app/src/main/java/com/example/notesapp/screens/Start.kt
@@ -0,0 +1,11 @@
+package com.example.notesapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController){
+    Text(text = "Start screen app")
+}
